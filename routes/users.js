bb73e0194abddbc7d993843336e3771a2aea7ddb
Irina <irinasensei@gmail.com>
2020-08-04 21:17:06 +0900

    created a post route but it doesn't work, socket hang up error

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const bodyParser = require("body-parser");
+const db = require("../model/helper");
+const { request } = require("../app");
 
+router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("this is Aimily");
 });
+
+// POST create user name
+router.post("/", async (req, res, next) => {
+  const { name, email } = req.body;
+  await db(`insert into users (name, email) values ('${name}, '${email});`);
+});
+
+//POST create goal with deadline
+
+//POST connect user and goal in database
+
+//GET all info on specific user
 
 module.exports = router;

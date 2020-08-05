58c6328d4a0ab6e87888381c827a793da361b5a6
Irina <irinasensei@gmail.com>
2020-08-05 15:54:40 +0900

    created the 3rd table and wrote all routes

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.use(bodyParser.json());
 
 });
 */
-// GET users temporary function to see what's being added to database
+// GET users temporary function to see what's being added to user database
 const getUsers = async (req, res, next) => {
   try {
     const results = await db(`SELECT *FROM users ORDER BY id ASC;`);
@@ -21,6 +21,18 @@ const getUsers = async (req, res, next) => {
   }
 };
 router.get("/", getUsers);
+
+// GET goals temporary function to see what's being added to goals database
+const getGoals = async (req, res, next) => {
+  try {
+    const results = await db(`SELECT *FROM goals ORDER BY id ASC;`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+router.get("/goals", getGoals);
+
 // POST create user name
 router.post("/", async (req, res, next) => {
   const { name, email } = req.body;
@@ -34,8 +46,32 @@ router.post("/", async (req, res, next) => {
 
 //POST create goal with deadline
 
+router.post("/goals", async (req, res, next) => {
+  const { goal, deadline, description } = req.body;
+  try {
+    await db(
+      `insert into goals (goal, deadline, description) values ("${goal}", "${deadline}", "${description}");`
+    );
+    getGoals(req, res);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 //POST connect user and goal in database
+//mysql> select users.name , goals.goal, goals.deadline, goals.description from users INNER JOIN goals ON users.id_goal=goals.id;
 
 //GET all info on specific user
+//mysql> select * from users_goals where name="Irina";
+const getUserById = async (req, res, next) => {
+  const { user_id } = req.params;
+  try {
+    await db(`SELECT *FROM users_goals where id="${user_id}";`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+router.get("/:id", getUserById);
 
 module.exports = router;
